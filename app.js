024ae94f0cee75494a735d0fd8d31606caf497fd
Dmytro <azarenkov.mintymint@gmail.com>
2025-10-03 14:32:51 -0400

    Added link to API docs on main page

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('<h1>Appointment API</h1><a href="/api-docs">Documentation</a>');
+});
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
